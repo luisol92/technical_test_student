e39efb5ca48b21fb3f84ae7700ff0e81c6309c27
Luis Solís <luisolcajusiju@gmail.com>
2025-02-09 09:18:38 -0500

    It add basic authentication

---
+++ student-back-api/app/Http/Middleware/BasicAuthentication.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class BasicAuthentication
+{
+    private const USER = "admin";
+    private const PASS = "admin";
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->hasHeader('Authorization') === false) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED);
+        }
+ 
+        $credentials = base64_decode(substr($request->header('Authorization'), 6));
+        list($username, $password) = explode(':', $credentials);
+ 
+        if ($username !== self::USER || $password !== self::PASS) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $next($request);
+    }
+}
--- student-back-api/routes/api.php
+++ student-back-api/routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GradeController;
 use App\Http\Controllers\StudentController;
+use App\Http\Middleware\BasicAuthentication;
 
-Route::post('/crear-alumno', [StudentController::class,'save']);
+Route::post('/crear-alumno', [StudentController::class,'save'])->middleware(BasicAuthentication::class);
 
-Route::get('/consultar-alumno/{id}', [StudentController::class,'search']);
+Route::get('/consultar-alumno/{id}', [StudentController::class,'search'])->middleware(BasicAuthentication::class);
 
-Route::post('/crear-grado', [GradeController::class,'save']);
+Route::post('/crear-grado', [GradeController::class,'save'])->middleware(BasicAuthentication::class);
