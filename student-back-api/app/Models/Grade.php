0d3bcc123d4514f08b5c9807bd866e5947191a9f
Luis Solís <luisolcajusiju@gmail.com>
2025-02-09 07:58:16 -0500

    It create Models

---
+++ student-back-api/app/Models/Grade.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Grade extends Model
+{
+    use HasFactory;
+    protected $table = 'grade';
+    protected $fillable = [
+        'name'
+    ];
+}
---
+++ student-back-api/app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $table = 'student';
+    protected $fillable = [
+        'name',
+        'birthdate',
+        'father',
+        'mother',
+        'grade_id',
+        'section',
+    ];
+}
